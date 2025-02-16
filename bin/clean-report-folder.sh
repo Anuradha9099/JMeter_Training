71cf6c8704b5e4b78e87d5312ac88dcec086fd42
AnuGitLab <piyumalanuradhapython@gmail.com>
2025-02-16 15:20:35 +0530

    added clean report folder bash file

---
+++ bin/clean-report-folder.sh
@@ -0,0 +1,7 @@
+# Remove the existing test-reports directory if it exists
+rm -rf test-reports
+
+# Create a new test-reports directory
+mkdir test-reports
+
+echo "Test reports directory has been reset."
